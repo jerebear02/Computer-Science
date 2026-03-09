f2453e98bdf03cdfcddb508d7dbb9d5154219753
jerebear02 <jeremiah.johnson2@students.snow.edu>
2026-03-08 18:36:48 -0600

    Add pyramid height input and printing logic
    Continuing to code in python has been a pleasant journey I am enjoying.

---
+++ mario.py
@@ -0,0 +1,9 @@
+height = 0
+while height < 1 or height > 8:
+    try:
+        height = int(input("Height: "))
+    except ValueError:
+        pass
+
+for i in range(1, height + 1):
+    print(" " * (height - i) + "#" * i + "  " + "#" * i)
